b852e869db64d5d21e39b387b066fc3e24fbd9e6
maxor5991 <aljoseguba1995@gmail.com>
2018-08-14 13:15:03 -0500

    LOL

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string hiddenWord = Console.ReadLine();
+            string shownWord = "";
+
+            Console.Clear();
+            //if (hiddenWord == "hola") {
+            //    Console.WriteLine("chau");
+            //}
+            //else if(hiddenWord== "chau"){
+            //    Console.WriteLine("Rayos me haz derrotado");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tendrias que haberme saludado...");
+            //}
+            for (int i=0; i <hiddenWord.Length; i++)
+            {
+                Console.WriteLine (shownWord);
+            }
         }
     }
 }
