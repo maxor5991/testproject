8a5fa2c58b1ae3543c12f0346aab10e04875e5ff
maxor5991 <aljoseguba1995@gmail.com>
2019-04-05 15:13:12 -0500

    Best mejora

---
+++ ajax/new_project.php
@@ -0,0 +1,55 @@
+<?php
+	include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
+	/*Inicia validacion del lado del servidor*/
+	if (empty($_POST['nombre'])) {
+           $errors[] = "Nombre vacío";
+        } else if (!empty($_POST['nombre'])){
+		/* Connect To Database*/
+		require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+		require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
+		// escaping, additionally removing everything that could be (html/javascript-) code
+		$nombre=mysqli_real_escape_string($con,(strip_tags($_POST["name_project"],ENT_QUOTES)));
+		$descripcion=mysqli_real_escape_string($con,(strip_tags($_POST["des_proj"],ENT_QUOTES)));
+        $date_added=date("Y-m-d H:i:s");
+        $date_end=date("Y-m-d H:i:s");
+		$sql="INSERT INTO projects (name_project, des_proj, date_added, date_end, id_task,) VALUES ('$name_project','$des_proj','$date_added','$date_end','$id_task')";
+		$query_new_insert = mysqli_query($con,$sql);
+			if ($query_new_insert){
+				$messages[] = "Se creo el proyecto correctamente";
+			} else{
+				$errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error($con);
+			}
+		} else {
+			$errors []= "Error desconocido.";
+		}
+		
+		if (isset($errors)){
+			
+			?>
+			<div class="alert alert-danger" role="alert">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>Error!</strong> 
+					<?php
+						foreach ($errors as $error) {
+								echo $error;
+							}
+						?>
+			</div>
+			<?php
+			}
+			if (isset($messages)){
+				
+				?>
+				<div class="alert alert-success" role="alert">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<strong>¡Bien hecho!</strong>
+						<?php
+							foreach ($messages as $message) {
+									echo $message;
+								}
+							?>
+				</div>
+				<?php
+			}
+
+?>
---
+++ footer.php
@@ -0,0 +1,47 @@
+<script>
+    $(function(){
+
+$('.validanumericos').keypress(function(e) {
+  if(isNaN(this.value + String.fromCharCode(e.charCode))) 
+   return false;
+})
+.on("cut copy paste",function(e){
+  e.preventDefault();
+});
+
+});
+</script>
+
+<div class="footer">
+            <div class="pull-right">
+           <!--           10GB of <strong>250GB</strong> Free.-->
+            </div>
+            <div>
+          <!--      <strong>Copyright</strong> Example Company &copy; 2014-2017-->
+            </div>
+        </div>
+
+    </div>
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+	
+
+<!-- Mainly scripts -->
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="js/inspinia.js"></script>
+<script src="js/plugins/pace/pace.min.js"></script>
+
+
+
+</body>
+
+</html>
