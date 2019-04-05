a2918cb1f1da94e3b68fa66ffeb61c5c034f4253
maxor5991 <aljoseguba1995@gmail.com>
2019-04-05 18:18:58 -0500

    Base de datos

--- simple_stock.sql
+++ simple_stock.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-12-2016 a las 11:38:12
--- Versión del servidor: 5.6.26
--- Versión de PHP: 5.6.12
+-- Tiempo de generación: 06-04-2019 a las 01:18:07
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `simple_stock`
+-- Base de datos: `gsnbd`
 --
 
 -- --------------------------------------------------------
@@ -26,21 +28,19 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `categorias`
 --
 
-CREATE TABLE IF NOT EXISTS `categorias` (
+CREATE TABLE `categorias` (
   `id_categoria` int(11) NOT NULL,
   `nombre_categoria` varchar(255) NOT NULL,
   `descripcion_categoria` varchar(255) NOT NULL,
   `date_added` datetime NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `categorias`
 --
 
 INSERT INTO `categorias` (`id_categoria`, `nombre_categoria`, `descripcion_categoria`, `date_added`) VALUES
-(1, 'Repuestos', 'Equipos para el hogar', '2016-12-19 00:00:00'),
-(4, 'Equipos', 'Equipos stihl', '2016-12-19 21:06:37'),
-(5, 'Accesorios', 'Accesorios stihl', '2016-12-19 21:06:39');
+(11, 'Camara', 'Test', '2019-04-05 00:26:04');
 
 -- --------------------------------------------------------
 
@@ -48,7 +48,7 @@ INSERT INTO `categorias` (`id_categoria`, `nombre_categoria`, `descripcion_categ
 -- Estructura de tabla para la tabla `historial`
 --
 
-CREATE TABLE IF NOT EXISTS `historial` (
+CREATE TABLE `historial` (
   `id_historial` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -58,13 +58,23 @@ CREATE TABLE IF NOT EXISTS `historial` (
   `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `historial`
+--
+
+INSERT INTO `historial` (`id_historial`, `id_producto`, `user_id`, `fecha`, `nota`, `referencia`, `cantidad`) VALUES
+(8, 7, 1, '2019-04-05 00:50:04', 'Alejandro agregÃ³ 100 producto(s) al inventario', '001', 100),
+(9, 7, 1, '2019-04-05 01:05:55', 'Alejandro eliminÃ³ 5 producto(s) del inventario', 'Entrega', 5),
+(10, 7, 1, '2019-04-05 01:06:14', 'Alejandro retirÃ³ 5 producto(s) del inventario', 'Entrega', 5),
+(11, 7, 1, '2019-04-05 01:06:24', 'Alejandro retirÃ³ 5 producto(s) del inventario', 'Entrega', 5);
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `products`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
+CREATE TABLE `products` (
   `id_producto` int(11) NOT NULL,
   `codigo_producto` char(20) NOT NULL,
   `nombre_producto` char(255) NOT NULL,
@@ -74,13 +84,51 @@ CREATE TABLE IF NOT EXISTS `products` (
   `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id_producto`, `codigo_producto`, `nombre_producto`, `date_added`, `precio_producto`, `stock`, `id_categoria`) VALUES
+(7, '001', 'Camara de seguridad', '2019-04-05 00:50:04', 1550, 85, 11);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `projects`
+--
+
+CREATE TABLE `projects` (
+  `id_project` int(11) NOT NULL,
+  `name_project` char(255) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `des_proj` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `id_task` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id_task` int(11) NOT NULL,
+  `name_task` char(255) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `id_project` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `user_id` int(11) NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
   `firstname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -88,14 +136,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
   `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
   `date_added` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `user_name`, `user_password_hash`, `user_email`, `date_added`) VALUES
-(1, 'Obed', 'Alvarado', 'admin', '$2y$10$MPVHzZ2ZPOWmtUUGCq3RXu31OTB.jo7M9LZ7PmPQYmgETSNn19ejO', 'admin@admin.com', '2016-12-19 15:06:00');
+(1, 'Alejandro', 'Guanilo', 'admin', '$2y$10$MPVHzZ2ZPOWmtUUGCq3RXu31OTB.jo7M9LZ7PmPQYmgETSNn19ejO', 'admin@admin.com', '2016-12-19 15:06:00'),
+(2, 'Test', 'Test', 'Test123', '$2y$10$xc2x3WpSbqonyDtK0dVtCejWMzt0VlK88Iss2Ys/iBY6SMvISkSAC', 'test@123.com', '2019-04-05 00:45:31');
 
 --
 -- Índices para tablas volcadas
@@ -122,6 +171,20 @@ ALTER TABLE `products`
   ADD UNIQUE KEY `codigo_producto` (`codigo_producto`);
 
 --
+-- Indices de la tabla `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id_project`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `id_task` (`id_task`);
+
+--
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id_task`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -137,22 +200,32 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index', AUTO_INCREMENT=3;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -162,6 +235,14 @@ ALTER TABLE `users`
 --
 ALTER TABLE `historial`
   ADD CONSTRAINT `fk_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `products` (`id_producto`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`id_task`) REFERENCES `tasks` (`id_task`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
