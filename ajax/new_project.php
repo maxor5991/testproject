d1b163c4aa16febabb1ca0f22d0404eacf223c1c
maxor5991 <aljoseguba1995@gmail.com>
2019-04-09 11:26:10 -0500

    Revert "Problem"
    This reverts commit b3b19f6b3b14f0f271fc6595a038d42648f1a0c6.

--- ajax/new_project.php
+++ ajax/new_project.php
@@ -1,14 +1,13 @@
 <?php
 	include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
 	/*Inicia validacion del lado del servidor*/
-	if (empty($_POST['nombre'])) {
+	if (empty($_POST['name_project'])) {
            $errors[] = "Nombre vacío";
-        } else if (!empty($_POST['nombre'])){
+        } else if (!empty($_POST['name_project'])){
 		/* Connect To Database*/
 		require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 		require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
 		// escaping, additionally removing everything that could be (html/javascript-) code
-<<<<<<< HEAD
 		$name_project=mysqli_real_escape_string($con,(strip_tags($_POST["name_project"],ENT_QUOTES)));
 		$des_project=mysqli_real_escape_string($con,(strip_tags($_POST["des_project"],ENT_QUOTES)));
 		$date_added=date("Y-m-d H:i:s");
@@ -17,13 +16,6 @@
 		$task_id=mysqli_real_escape_string($con,(strip_tags($_POST["task_id"],ENT_QUOTES)));
 		$sql="INSERT INTO projects (name_project, des_project, date_added, date_end, user_id, task_id ) 
 		VALUES ('$name_project','$des_project','$date_added', '$date_end', '$user_id', '$task_id')";
-=======
-		$nombre=mysqli_real_escape_string($con,(strip_tags($_POST["name_project"],ENT_QUOTES)));
-		$descripcion=mysqli_real_escape_string($con,(strip_tags($_POST["des_proj"],ENT_QUOTES)));
-        $date_added=date("Y-m-d H:i:s");
-        $date_end=date("Y-m-d H:i:s");
-		$sql="INSERT INTO projects (name_project, des_proj, date_added, date_end, id_task,) VALUES ('$name_project','$des_proj','$date_added','$date_end','$id_task')";
->>>>>>> parent of c916fa0... Insercion de proyectos
 		$query_new_insert = mysqli_query($con,$sql);
 			if ($query_new_insert){
 				$messages[] = "El proyecto fue creado satisfactoriamente.";
